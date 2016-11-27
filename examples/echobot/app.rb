13f8ce0a3fd732f44573ef4fa75b0ab55aa17ac7
mizokami <r.mizokami@gmail.com>
2016-11-27 11:35:12 +0900

    Fix example

--- examples/echobot/app.rb
+++ examples/echobot/app.rb
@@ -4,7 +4,8 @@ require 'line/bot'  # gem 'line-bot-api'
 def client
   @client ||= Line::Bot::Client.new { |config|
     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    config.channel_token  = ENV["LINE_CHANNEL_TOKEN"]
+    config.endpoint       = ENV["LINE_ENDPOINT"]
   }
 end
 
